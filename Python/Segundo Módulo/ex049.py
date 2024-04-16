442c80897894a224f68d5af399d8de17488fe30e
Ulisses Caldas da Silva <140160383+ulissescaldas@users.noreply.github.com>
2024-04-16 08:54:30 -0300

    Estudos  16042024
    Resolução dos exercícios da aula 013 no dia 16/04/2024

---
+++ Python/Segundo Módulo/ex049.py
@@ -0,0 +1,4 @@
+print('Exercício 49 – Tabuada v.2.0')
+vNumero = int(input('Digite um número: '))
+for i in range(1, 11):
+    print('{} x {} = {}'.format(vNumero, i, vNumero * i))
---
+++ Python/Segundo Módulo/ex050.py
@@ -0,0 +1,16 @@
+print('''Exercício Python 50: 
+Desenvolva um programa que leia seis números inteiros e mostre a soma apenas daqueles que forem pares.
+Se o valor digitado for ímpar, desconsidere-o.''')
+vSoma = 0
+vAcumulador = []
+for i in range(1, 7):
+    vNumero = int(input('Digite o número {}: '.format(i)))
+    if vNumero % 2 == 0:
+        vSoma += vNumero
+        vAcumulador.append(vNumero)
+if vSoma == 0:
+    print('Nenhum número par foi digitado')
+else:
+    print('Números pares: {} '.format(vAcumulador))
+    print('A soma dos números pares é {}'.format(vSoma))
+
