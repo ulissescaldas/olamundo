be6f3927aeadbed23d932afecead762f2a9efd8c
Ulisses Caldas da Silva <140160383+ulissescaldas@users.noreply.github.com>
2024-05-15 09:44:11 -0300

    estudos 150520240944

--- Python/Terceiro Módulo/ex094.py
+++ Python/Terceiro Módulo/ex094.py
@@ -10,8 +10,37 @@ print('-=-' * 50)
 
 vCadastro = {}
 vLista = []
+vSoma = 0
 
 while True:
-    vCadastro['nome'] = str(input('Nome: '))
-    break
-print(vCadastro)
+    vCadastro['nome'] = str(input('\nNome: '))
+    while True:
+        vCadastro['sexo'] = str(input('Sexo [M/F]: ')).upper().strip()[0]
+        if vCadastro['sexo'] in 'MF':
+            break
+        print('Opção invalida!')
+    vCadastro['idade'] = int(input('Idade: '))
+    vLista.append(vCadastro.copy())
+    vCadastro.clear()
+    while True:
+        vOpcao = str(input('Deseja continuar? [S/N] ')).upper().strip()[0]
+        if vOpcao in 'SN':
+            break
+        print('Opção invalida!')
+    if vOpcao == 'N':
+        break
+vTotal = len(vLista)
+print(f'A) Ao total temos {vTotal} pessoas cadastradas')
+for i in vLista:
+    vSoma += i['idade']
+vMedia = vSoma/vTotal
+print(f'B) A média de idade é de {vMedia:.2f}')
+print(f'C) As cadastradas foram ', end='')
+for i in vLista:
+    if i['sexo'] == 'F':
+        print(f'{i['nome']} ', end='')
+print('\nD) Lista das Pessoas que estão acima da média:')
+for i in vLista:
+    if i['idade'] >= vMedia:
+        print(f'      Nome = {i['nome']:<15}; Sexo = {i['sexo']:<8}; Idade = {i['idade']:<8}')
+print('\nPrograma Finalizado!!!')
