22237d73d9d72c8c61545bfd0364a98eb08a06de
Ulisses Caldas da Silva <140160383+ulissescaldas@users.noreply.github.com>
2024-05-17 09:11:54 -0300

    estudo 170520240911

--- Python/Terceiro Módulo/ex083.py
+++ Python/Terceiro Módulo/ex083.py
@@ -1,6 +1,6 @@
 print('-=-' * 50)
-print('''Exercício Python 083: 
-Crie um programa onde o usuário digite uma expressão qualquer que use parênteses. 
+print('''Exercício Python 083:
+Crie um programa onde o usuário digite uma expressão qualquer que use parênteses.
 Seu aplicativo deverá analisar se a expressão passada está com os parênteses abertos e fechados na ordem correta.''')
 print('-=-' * 50)
 
